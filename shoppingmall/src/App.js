4b39ba608a31abd1cf5095482ac178dbe75f0222
Tharun Kumar L <154898153+TharunKumarL@users.noreply.github.com>
2024-08-31 23:59:54 +0530

    commit

--- shoppingmall/src/App.js
+++ shoppingmall/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from './shoppingFolder/components/Header';
+import NavBar from './shoppingFolder/components/NavBar';
 function App() {
   return (
     <div>
       <Header/>
+      <NavBar/>
     </div>
   );
 }
---
+++ shoppingmall/src/shoppingFolder/components/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div class="NavBar">
+    <div class="body">
+        <h1>Home</h1>
+    </div>
+    <div class="main">
+        <div class="second-body">
+            <div class="thing">
+                <span class="dot-2-left"></span> <span class="dot-left"></span> <h3> Sports </h3> <span class="dot-right"></span> <span class="dot-2-right"></span> 
+            </div>
+            <div class="thing">
+                <span class="dot-2-left"></span> <span class="dot-left"></span> <h3> Accessories </h3> <span class="dot-right"></span> <span class="dot-2-right"></span>
+               </div>
+               <div class="thing">
+                <span class="dot-2-left"></span> <span class="dot-left"></span> <h3> Cinemas </h3> <span class="dot-right"></span> <span class="dot-2-right"></span>
+               </div>
+        </div>
+    </div>
+    </div>
+  );
+};
+
+export default NavBar;
